35e3c2f323fe11ec129f5bb189be09ea7017f067
Per Zetterlund <per@pz.se>
2017-01-06 07:01:22 +0100

    Inverse kinematics

--- kinematics.py
+++ kinematics.py
@@ -37,17 +37,36 @@ class Link:
 
 
 
-class ArticulatedArmForward:
+class ArticulatedArm:
     def __init__(self):
         l1 = 8
         l2 = 20
         l3 = 25
 
         self.L1 = Link(math.radians(90), 0, l1, None)
-        self.L2 = Link(math.radians(0), l2, 0, None)
+        self.L2 = Link(math.radians(180 ), l2, 0, None)
         self.L3 = Link(math.radians(-90), l3, 0, None)
 
-    def calculate(self, theta1, theta2, theta3):
+    def calculate_forward(self, theta1, theta2, theta3):
         m = self.L1.calculate(theta1) * self.L2.calculate(theta2) * self.L3.calculate(theta3)
 
-        return m @ np.array([0, 0, 0, 1])
+        return (m @ np.array([0, 0, 0, 1])).transpose()
+
+    def calculate_inverse(self, X, Y, Z):
+        def angle_from_sides(a, b, c):
+            return math.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b))
+
+        R2 = X ** 2 + Y ** 2
+        R = math.sqrt(R2)
+
+        Zprim = Z - self.L1.d
+
+        S2 = Zprim ** 2 + R2
+        S = math.sqrt(S2)
+
+        theta0 = math.atan2(Y, X)
+        theta1 = angle_from_sides(self.L2.a, S, self.L3.a)
+        theta2 = math.pi - angle_from_sides(self.L2.a, self.L3.a, S)
+        theta_h = math.atan2(Zprim, R)
+
+        return tuple([math.degrees(theta0), math.degrees(theta1 + theta_h), math.degrees(theta2)])
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #import input
 import kinematics
 import math
+import numpy as np
 
 def main():
-    f = kinematics.ArticulatedArmForward()
-    print(f.calculate(0, math.radians(0), math.radians(-1)))
+    f = kinematics.ArticulatedArm()
+    forward = f.calculate_forward(math.radians(0), math.radians(1), math.radians(1))
+    np.set_printoptions(precision=4)
+    np.set_printoptions(suppress=True)
+    print(forward)
+    inverse = f.calculate_inverse(forward[0], forward[1], forward[2])
+    print(np.array(inverse))
 
 if __name__ == "__main__":
     main()
